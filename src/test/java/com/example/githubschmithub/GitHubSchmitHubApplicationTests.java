3210dc029bd020a3f587cded38cde7976c7cf83c
Joakim R. S. <jose0001@stud.kea.dk>
2025-02-24 11:25:47 +0100

    Initial commit

---
+++ src/test/java/com/example/githubschmithub/GitHubSchmitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubschmithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubSchmitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
